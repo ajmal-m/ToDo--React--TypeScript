abb42e12a200fe16b53fb4c7e172522e1b9c9dc3
ajmal5410347 <ajmal@airtory.com>
2025-02-18 23:02:54 +0530

    Delet update

--- src/App.tsx
+++ src/App.tsx
@@ -25,15 +25,14 @@ function App() {
 
 
 
-  const deleteTodo = (key : string | number) => {
+  const deleteTodo = (key: string | number) => {
     try {
-      setTodoList((prev) => {
-        return prev.filter((item) => (item?.id != key))
-      })
+      setTodoList((prev) => prev.filter((item) => item.id !== key));
     } catch (error) {
-      console.log(error);
+      console.error("Error deleting todo:", error);
     }
-  }
+  };
+  
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 bg-gray-800 dark:bg-gray-800">
       <div className="flex flex-col gap-[8px]">
@@ -58,9 +57,9 @@ function App() {
           {
             todoList.map((item) => {
               return(
-                <div className="w-full flex justify-between px-[8px] py-[4px] bg-[black] text-[white] rounded" key={item.key}>
+                <div className="w-full flex justify-between px-[8px] py-[4px] bg-[black] text-[white] rounded" key={item.id}>
                   <p>{item?.value}</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => deleteTodo(item?.key)} className="cursor-pointer" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => deleteTodo(item?.id)} className="cursor-pointer" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/></svg>
                 </div>
               )
             })
