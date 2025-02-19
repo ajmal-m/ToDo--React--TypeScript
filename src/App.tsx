5197c25546cf530e79c166b7a0727948cb5a94d0
ajmal5410347 <ajmal@airtory.com>
2025-02-19 08:57:34 +0530

    Form component update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import { Form } from "./components/Form";
 
 type ListItem = {
-  id: string | number,
+  id: number,
   value: string
 }
 
@@ -9,28 +10,8 @@ function App() {
   const [text , setText]= useState<string>("");
   const [todoList, setTodoList] = useState<ListItem[]>([]);
 
-  const handleSubmit = (e : React.FormEvent) => {
-    try {
-      e.preventDefault();
-      setTodoList((prev) => (
-        [
-          ...prev,
-          {
-            id:  Date.now(),
-            value: text
-          }
-        ]
-      ));
-      setText("");
-    } catch (error) {
-      console.log(error);
-      
-    }
-  }
 
-
-
-  const deleteTodo = (key: string | number) => {
+  const deleteTodo = (key: number): void => {
     try {
       setTodoList((prev) => prev.filter((item) => item?.id !== key));
     } catch (error) {
@@ -41,21 +22,7 @@ function App() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 bg-gray-800 dark:bg-gray-800">
       <div className="flex flex-col gap-[8px]">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-[8px]">
-          <input 
-            type="text" 
-            className="
-              bg-[#f5f5f5] dark:bg-[#f5f5f5] border 
-              rounded p-[4px] focus:outline-none focus:ring-0 
-              text-[#242424] dark:text-[#242424]
-            " 
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            name="todoText" 
-            id="todoText" 
-          />
-          <button type="submit"  className="w-full bg-[#644dff] py-[4px] rounded">ADD</button>
-        </form>
+        <Form  text={text} setTodoList={setTodoList}  setText={setText}/>
 
         <div className="max-h-[400px] overflow-y-auto flex flex-col gap-[8px]">
 
@@ -64,7 +31,15 @@ function App() {
               return(
                 <div className="w-full flex justify-between px-[8px] py-[4px] bg-[black] text-[white] rounded" key={item.id}>
                   <p>{item?.value}</p>
-                  <svg xmlns="http://www.w3.org/2000/svg" onClick={() => deleteTodo(item?.id)} className="cursor-pointer" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed"><path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/></svg>
+                  <button onClick={() => deleteTodo(item?.id)} >
+                    <svg 
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="cursor-pointer" height="20px" viewBox="0 -960 960 960" width="20px" 
+                      fill="#e8eaed"
+                    >
+                      <path d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z"/>
+                    </svg>
+                  </button>
                 </div>
               )
             })
---
+++ src/components/Form.tsx
@@ -0,0 +1,53 @@
+
+
+type FormParams = {
+    text: string,
+    setText: ( value : string) => void,
+    setTodoList: ( callback: ( value : ListItem[]) => ListItem[] ) => void
+}
+
+type ListItem = {
+    id: number,
+    value: string
+}
+
+export const Form = ({ text  , setText, setTodoList} :FormParams ) => {
+
+    const handleSubmit = (e : React.FormEvent): void => {
+        try {
+          e.preventDefault();
+          setTodoList((prev) => (
+            [
+              ...prev,
+              {
+                id:  Date.now(),
+                value: text
+              }
+            ]
+          ));
+          setText("");
+        } catch (error) {
+          console.log(error);
+          
+        }
+    }
+    return(
+        <>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-[8px]">
+            <input 
+                type="text" 
+                className="
+                bg-[#f5f5f5] dark:bg-[#f5f5f5] border 
+                rounded p-[4px] focus:outline-none focus:ring-0 
+                text-[#242424] dark:text-[#242424]
+                " 
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                name="todoText" 
+                id="todoText" 
+            />
+            <button type="submit"  className="w-full bg-[#644dff] py-[4px] rounded">ADD</button>
+        </form>
+        </>
+    )
+}
